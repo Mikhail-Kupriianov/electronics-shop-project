392e53faa9bd0991e9d6885fdcc9fbcfcf4a09e8
Mkupr <mkupr@outlook.com>
2023-03-14 23:25:36 +0300

    add test_item.py

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,14 @@
+import pytest
+from src.item import Item
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+
+
+@pytest.fixture
+def test_item():
+    return Item("Ноутбук", 20000, 5)
+
+
+def test_init(test_item):
+    assert test_item.name == "Ноутбук"
+    assert test_item.price == 20000
+    assert test_item.quantity == 5
