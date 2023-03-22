a5ac00417ffb9de2652e600bbb0581b075dfd5de
Mkupr <mkupr@outlook.com>
2023-03-22 17:58:49 +0300

    edit test_item.py

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,5 +1,6 @@
 import pytest
 from src.item import Item
+
 """Здесь надо написать тесты с использованием pytest для модуля item."""
 
 
@@ -24,3 +25,29 @@ def test_apply_discount(test_item):
     test_item.pay_rate = 0.8
     test_item.apply_discount()
     assert test_item.price == 16000.0
+
+
+def test_name_setter(test_item):
+    test_item.name = "Notebook"
+    assert test_item.name == "Notebook"
+
+
+def test_name_getter(test_item):
+    assert test_item.name == "Ноутбук"
+
+
+def test_instantiate_from_csv():
+    Item.all.clear()
+    Item.instantiate_from_csv()
+    assert Item.all[0].name == "Смартфон"
+    assert Item.all[1].price == "1000"
+    assert Item.all[4].quantity == "5"
+    assert len(Item.all) == 5
+
+
+def test_string_to_number():
+    assert Item.string_to_number("5") == 5
+    assert Item.string_to_number('5.0') == 5
+    assert Item.string_to_number('5.5') == 5
+
+    """ pytest --cov=src --cov-report=html """
