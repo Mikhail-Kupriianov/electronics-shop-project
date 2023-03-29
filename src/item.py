8ffbd50adc4f04d921ca4ce1ac684181d13e3572
Mkupr <mkupr@outlook.com>
2023-03-29 21:12:15 +0300

    edit_item.py

--- src/item.py
+++ src/item.py
@@ -71,3 +71,8 @@ class Item:
     def __repr__(self):
         return f"{self.__class__.__name__}('{self.__name}', {self.price}, {self.quantity})"
 
+    def __add__(self, other):
+        if not isinstance(other, Item):
+            raise ValueError('Складывать можно только объекты Item и дочерние от них.')
+        return self.quantity + other.quantity
+
