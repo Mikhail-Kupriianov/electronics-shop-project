4f01462181b9b0c6efd379d3a8bcae47a976cc5a
Mkupr <mkupr@outlook.com>
2023-04-04 08:55:58 +0300

    edit main.py

--- homework-6/main.py
+++ homework-6/main.py
@@ -2,11 +2,11 @@ from src.item import Item
 
 if __name__ == '__main__':
     # Файл items.csv отсутствует.
-    Item.csv_file_name = 'items_b.csv'
-    Item.instantiate_from_csv()
+
+    Item.instantiate_from_csv('items_b.csv')
     # FileNotFoundError: Отсутствует файл item.csv
 
     # В файле items.csv удалена последняя колонка.
-    Item.csv_file_name = 'items_bad.csv'
-    Item.instantiate_from_csv()
+
+    Item.instantiate_from_csv('items_bad.csv')
     # InstantiateCSVError: Файл item.csv поврежден
